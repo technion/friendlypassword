e739d27d14cad0a44442634b28ae802036884787
Technion <technion@lolware.net>
2021-11-18 20:36:20 +1100

    More robust RNG and fetch()

--- src/DiceGenerator.tsx
+++ src/DiceGenerator.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 
-const getRandomDice = (min: number, max: number) => {
+const getRandomDice = (min: number, max: number): number => {
   // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
   // https://stackoverflow.com/questions/2509679/how-to-generate-a-random-integer-number-from-within-a-range
+  // Design Requirements: Avoid floating point
   let array = new Uint32Array(1);
   const range = 1 + max - min;
-  const buckets = 0xffffffff / range; // RAND_MAX / RANGE
-  const limit: number = buckets * range;
-  let random: number = window.crypto.getRandomValues(array)[0];
+  const buckets = Math.floor(0xffffffff / range); // RAND_MAX / RANGE
+  const limit = buckets * range;
+  let random = window.crypto.getRandomValues(array)[0];
   while (random >= limit) {
     random = window.crypto.getRandomValues(array)[0];
   }
-  return Math.floor(min + random / buckets);
+  return min + (random % range)
 };
 
 const DiceGenerator: React.FC = () => {
@@ -27,12 +28,28 @@ const DiceGenerator: React.FC = () => {
 
   useEffect(() => {
     const fetchlist = async () => {
-      const response = await fetch("bip-0039.txt");
-      if (!response.ok) {
-        throw new Error("Invalid response from server fetch");
+      const controller = new AbortController();
+      const signal = controller.signal;
+      const timeout = setTimeout(() => controller.abort(), 2000);
+      let response;
+      try {
+        response = await fetch("bip-0039.txt", {signal});
+        if (!response.ok) {
+          throw new Error("Invalid response from server fetch");
+        }
+        const wordlist = await response.text();
+      } catch(e) {
+        if (e.name === 'AbortError') {
+          alert("Download timed out")
+          throw new Error("Download timed out");
+        } else {
+          alert(e)
+          throw new Error("Failed to fetch url:" + e)
+        }
+      } finally {
+        clearTimeout(timeout);
       }
-      const wordlist = await response.text();
-      setDiceList(wordlist.trim().split("\n"));
+      setDiceList(Object.freeze(wordlist.trim().split("\n")));
     };
 
     fetchlist();
@@ -42,15 +59,15 @@ const DiceGenerator: React.FC = () => {
     return <b>Loading wordlist..</b>;
   }
 
-  let secret = "";
+  let secret = new Array();
   for (let i = 0; i < numwords(); i++) {
     let r = getRandomDice(0, getDiceList.length - 1);
-    secret += getDiceList[r] + "-";
+    secret = secret.concat(getDiceList[r]);
   }
 
-  secret += getRandomDice(0, 99);
+  secret = secret.concat(getRandomDice(0, 99));
 
-  return <b>{secret}</b>;
+  return <b>{secret.join("-")}</b>;
 };
 
 export default DiceGenerator;
